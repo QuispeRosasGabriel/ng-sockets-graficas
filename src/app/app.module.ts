0d8f57f9c8f7906fae8c03d7923ac26d35575a60
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-07-29 12:19:19 -0500

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { ChartsModule } from 'ng2-charts';
+import { GraficasComponent } from './components/graficas/graficas.component';
+const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} };
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, GraficasComponent],
+  imports: [BrowserModule, SocketIoModule.forRoot(config), ChartsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/graficas/graficas.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
+
+@Component({
+  selector: 'app-graficas',
+  templateUrl: './graficas.component.html',
+  styleUrls: ['./graficas.component.css'],
+})
+export class GraficasComponent implements OnInit {
+  public lineChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Ventas' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+  ];
+
+  public lineChartLabels: Label[] = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
